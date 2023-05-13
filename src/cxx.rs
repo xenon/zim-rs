fc4ac1ac35cdf48cd6789f695a050bda77c3533f
Matthieu Gautier <dev@mgautier.fr>
2023-05-13 23:58:53 +0000

    Blob improvements

--- src/cxx.rs
+++ src/cxx.rs
@@ -1,5 +1,3 @@
-use std::ffi::CStr;
-
 use cxx::{CxxString, CxxVector, UniquePtr};
 
 pub(crate) fn string_from_ptr(ptr: UniquePtr<CxxString>) -> Result<String, ()> {
@@ -19,14 +17,4 @@ pub(crate) fn vec_string_from_ptr(ptr: UniquePtr<CxxVector<CxxString>>) -> Resul
             .map(|s| s.to_string())
             .collect::<Vec<String>>()),
     }
-}
-
-pub(crate) unsafe fn string_from_char_array(chars: *const i8) -> Result<String, ()> {
-    if chars.is_null() {
-        return Err(());
-    }
-    CStr::from_ptr(chars)
-        .to_str()
-        .map_err(|_| ())
-        .map(|str| str.to_string())
 }
